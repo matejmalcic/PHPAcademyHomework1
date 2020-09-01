802b7e7186090f14ea4eb0a2e31deee85e71eb49
Matej Malcic <matej.malcic3@gmail.com>
2020-09-01 12:34:40 +0200

    setup and top bar

--- index.php
+++ index.php
@@ -1,1 +1,33 @@
-Hello
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Academy 2020</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/css/foundation.min.css">
+</head>
+<body>
+    <div class="grid-container">
+        <div class="grid-x">
+            <div class="large-12 small-12 columns">
+                <div class="callout">
+                    <div class="top-bar">
+                        <div class="top-bar-left">
+                            <ul class="dropdown menu" data-dropdown-menu>
+                            <li class="menu-text">PHP Academy Homework 1</li>
+                            <li><a href="#">One</a></li>
+                            <li><a href="#">Two</a></li>
+                            <li><a href="#">Three</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.min.js"></script>
+    <script src="js/app.js"></script>
+</body>
+</html>
